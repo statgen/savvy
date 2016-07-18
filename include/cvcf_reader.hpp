7947a301bd574638ad46b26dfb47679658f8289b
jonathonl <jonathonl@users.noreply.github.com>
2016-07-18 16:12:28 -0400

    Replace for_each methods in cvcf marker with iterators.

--- include/cvcf_reader.hpp
+++ include/cvcf_reader.hpp
@@ -15,21 +15,86 @@ namespace vc
     class marker
     {
     public:
-      bool has_alt_at(std::uint64_t sample_off, std::uint8_t ploidy_off) const;
-      bool has_ref_at(std::uint64_t sample_off, std::uint8_t ploidy_off) const;
-      bool is_missing_at(std::uint64_t sample_off, std::uint8_t ploidy_off) const;
-      allele_status operator()(std::uint64_t sample_off, std::uint8_t ploidy_off) const;
-      void for_each_allele(const std::function<void(std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn);
-      void for_each_missing(const std::function<void(std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn);
-      void for_each_non_ref(const std::function<void(allele_status status, std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn);
-      double calculate_allele_frequency() const;
-    private:
-      struct hap_pair
+      struct sparse_allele
       {
         std::uint64_t offset;
-        bool is_allele;
+        bool is_missing;
       };
-      std::vector<hap_pair> non_zero_haplotypes_;
+
+      class non_ref_iterator
+      {
+      public:
+        typedef non_ref_iterator self_type;
+        typedef std::ptrdiff_t difference_type;
+        typedef allele_status value_type;
+        typedef const value_type& reference;
+        typedef const value_type* pointer;
+        typedef std::forward_iterator_tag iterator_category;
+      private:
+        static const value_type const_is_missing;
+        static const value_type const_has_alt;
+      public:
+
+        non_ref_iterator(const std::vector<sparse_allele>::const_iterator& ptr) : ptr_(ptr) {}
+        self_type& operator--(){ --ptr_; return *this; }
+        self_type operator--(int) { self_type r = *this; --ptr_; return r; }
+        self_type& operator++(){ ++ptr_; return *this; }
+        self_type operator++(int) { self_type r = *this; ++ptr_; return r; }
+        reference operator*() { return (ptr_->is_missing ? const_is_missing : const_has_alt); }
+        pointer operator->() { return (ptr_->is_missing ? &const_is_missing : &const_has_alt); }
+        bool operator==(const self_type& rhs) { return ptr_ == rhs.ptr_; }
+        bool operator!=(const self_type& rhs) { return ptr_ != rhs.ptr_; }
+
+        std::uint64_t offset() const { return ptr_->offset; }
+      private:
+        std::vector<sparse_allele>::const_iterator ptr_;
+      };
+
+      class haplotype_iterator
+      {
+      public:
+        typedef haplotype_iterator self_type;
+        typedef std::ptrdiff_t difference_type;
+        typedef allele_status value_type;
+        typedef const value_type& reference;
+        typedef const value_type* pointer;
+        typedef std::forward_iterator_tag iterator_category;
+      private:
+        static const value_type const_is_missing;
+        static const value_type const_has_ref;
+        static const value_type const_has_alt;
+      public:
+        haplotype_iterator(const std::vector<sparse_allele>::const_iterator& ptr, const std::vector<sparse_allele>::const_iterator& ptr_end) : ptr_(ptr), ptr_end_(ptr_end) {}
+        void increment()
+        {
+          if (ptr_ != ptr_end_ && i_ == ptr_->offset)
+            ++ptr_;
+          ++i_;
+        }
+        self_type& operator++(){ increment(); return *this; }
+        self_type operator++(int) { self_type r = *this; increment(); return r; }
+        reference operator*()
+        {
+          if (ptr_ != ptr_end_ && i_ == ptr_->offset)
+            return (ptr_->is_missing ? const_is_missing : const_has_alt);
+          return const_has_ref;
+        }
+        pointer operator->() { return &(haplotype_iterator::operator*()); }
+        bool operator==(const self_type& rhs) { return i_ == rhs.i_; }
+        bool operator!=(const self_type& rhs) { return i_ != rhs.i_; }
+
+        std::uint64_t offset() const { return i_; }
+      private:
+        std::size_t i_ = 0;
+        std::vector<sparse_allele>::const_iterator ptr_;
+        const std::vector<sparse_allele>::const_iterator ptr_end_;
+      };
+
+      non_ref_iterator non_ref_begin() const;
+      non_ref_iterator non_ref_end() const;
+      double calculate_allele_frequency() const;
+    private:
+      std::vector<sparse_allele> non_zero_haplotypes_;
       std::uint8_t ploidy_level_;
       std::uint64_t sample_count_;
     };
--- src/cvcf_reader.cpp
+++ src/cvcf_reader.cpp
@@ -5,40 +5,57 @@ namespace vc
   namespace cvcf
   {
     //================================================================//
-    void marker::for_each_allele(const std::function<void(std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn)
+//    void marker::for_each_allele(const std::function<void(std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn)
+//    {
+//      std::uint64_t haplotype_offset = 0;
+//      for (auto it = non_zero_haplotypes_.begin(); it != non_zero_haplotypes_.end(); ++it)
+//      {
+//        haplotype_offset += it->offset;
+//        if (it->is_allele)
+//        {
+//          fn(haplotype_offset / ploidy_level_, (std::uint8_t)(haplotype_offset % ploidy_level_));
+//        }
+//      }
+//    }
+//
+//    void marker::for_each_missing(const std::function<void(std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn)
+//    {
+//      std::uint64_t haplotype_offset = 0;
+//      for (auto it = non_zero_haplotypes_.begin(); it != non_zero_haplotypes_.end(); ++it)
+//      {
+//        haplotype_offset += it->offset;
+//        if (!(it->is_allele))
+//        {
+//          fn(haplotype_offset / ploidy_level_, (std::uint8_t)(haplotype_offset % ploidy_level_));
+//        }
+//      }
+//    }
+//
+//    void marker::for_each_non_ref(const std::function<void(allele_status status, std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn)
+//    {
+//      std::uint64_t haplotype_offset = 0;
+//      for (auto it = non_zero_haplotypes_.begin(); it != non_zero_haplotypes_.end(); ++it)
+//      {
+//        haplotype_offset += it->offset;
+//        fn(it->is_allele ? allele_status::has_alt : allele_status::is_missing, haplotype_offset / ploidy_level_, (std::uint8_t)(haplotype_offset % ploidy_level_));
+//      }
+//    }
+
+    const marker::non_ref_iterator::value_type marker::non_ref_iterator::const_is_missing = allele_status::is_missing;
+    const marker::non_ref_iterator::value_type marker::non_ref_iterator::const_has_alt = allele_status::has_alt;
+
+    const marker::haplotype_iterator::value_type marker::haplotype_iterator::const_is_missing = allele_status::is_missing;
+    const marker::haplotype_iterator::value_type marker::haplotype_iterator::const_has_ref = allele_status::has_ref;
+    const marker::haplotype_iterator::value_type marker::haplotype_iterator::const_has_alt = allele_status::has_alt;
+
+    marker::non_ref_iterator marker::non_ref_begin() const
     {
-      std::uint64_t haplotype_offset = 0;
-      for (auto it = non_zero_haplotypes_.begin(); it != non_zero_haplotypes_.end(); ++it)
-      {
-        haplotype_offset += it->offset;
-        if (it->is_allele)
-        {
-          fn(haplotype_offset / ploidy_level_, (std::uint8_t)(haplotype_offset % ploidy_level_));
-        }
-      }
+      return non_ref_iterator(non_zero_haplotypes_.begin());
     }
 
-    void marker::for_each_missing(const std::function<void(std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn)
+    marker::non_ref_iterator marker::non_ref_end() const
     {
-      std::uint64_t haplotype_offset = 0;
-      for (auto it = non_zero_haplotypes_.begin(); it != non_zero_haplotypes_.end(); ++it)
-      {
-        haplotype_offset += it->offset;
-        if (!(it->is_allele))
-        {
-          fn(haplotype_offset / ploidy_level_, (std::uint8_t)(haplotype_offset % ploidy_level_));
-        }
-      }
-    }
-
-    void marker::for_each_non_ref(const std::function<void(allele_status status, std::uint64_t sample_off, std::uint8_t ploidy_off)>& fn)
-    {
-      std::uint64_t haplotype_offset = 0;
-      for (auto it = non_zero_haplotypes_.begin(); it != non_zero_haplotypes_.end(); ++it)
-      {
-        haplotype_offset += it->offset;
-        fn(it->is_allele ? allele_status::has_alt : allele_status::is_missing, haplotype_offset / ploidy_level_, (std::uint8_t)(haplotype_offset % ploidy_level_));
-      }
+      return non_ref_iterator(non_zero_haplotypes_.end());
     }
 
     double marker::calculate_allele_frequency() const
