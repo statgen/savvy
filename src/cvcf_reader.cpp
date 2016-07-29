dfc3bd3985c002e46f836c3b19b4888c71ced4a4
jonathonl <jonathonl@users.noreply.github.com>
2016-07-29 17:09:59 -0400

    Fixes cvcf marker read.

--- src/cvcf_reader.cpp
+++ src/cvcf_reader.cpp
@@ -86,33 +86,38 @@ namespace vc
 
     bool marker::read(marker& destination, std::istream& is)
     {
-//      std::istreambuf_iterator<char> input_it(is);
-//      std::istreambuf_iterator<char> end_it;
-//      destination.position_ = varint_decode(input_it, end_it);
-//
-//      destination.id_.resize(varint_decode(input_it, end_it));
-//      if (destination.id_.size())
-//        is.read(&destination.id_[0], destination.alt_.size());
-//
-//      destination.ref_.resize(varint_decode(input_it, end_it));
-//      if (destination.ref_.size())
-//        is.read(&destination.ref_[0], destination.ref_.size());
-//
-//      destination.alt_.resize(varint_decode(input_it, end_it));
-//      if (destination.alt_.size())
-//        is.read(&destination.alt_[0], destination.alt_.size());
-//
-//      std::uint64_t offset = 0;
-//      destination.non_zero_haplotypes_.reserve(varint_decode(input_it, end_it));
-//      for (auto it = destination.non_zero_haplotypes_.begin(); it != destination.non_zero_haplotypes_.end(); ++it,++offset)
-//      {
-//        std::uint8_t allele = 0;
-//        offset += one_bit_prefixed_varint::decode(allele, input_it);
-//        it->offset = offset;
-//        it->status = (allele ? allele_status::has_alt : allele_status::is_missing);
-//      }
+      std::istreambuf_iterator<char> end_it;
+      varint_decode(std::istreambuf_iterator<char>(is), end_it, destination.position_);
 
-      return true;
+      std::uint64_t sz;
+      varint_decode(std::istreambuf_iterator<char>(is), end_it, sz);
+      destination.id_.resize(sz);
+      if (destination.id_.size())
+        is.read(&destination.id_[0], destination.alt_.size());
+
+      varint_decode(std::istreambuf_iterator<char>(is), end_it, sz);
+      destination.ref_.resize(sz);
+      if (destination.ref_.size())
+        is.read(&destination.ref_[0], destination.ref_.size());
+
+      varint_decode(std::istreambuf_iterator<char>(is), end_it, sz);
+      destination.alt_.resize(sz);
+      if (destination.alt_.size())
+        is.read(&destination.alt_[0], destination.alt_.size());
+
+      std::uint64_t total_offset = 0;
+      destination.non_zero_haplotypes_.reserve(sz);
+      for (auto it = destination.non_zero_haplotypes_.begin(); it != destination.non_zero_haplotypes_.end(); ++it,++total_offset)
+      {
+        std::uint8_t allele;
+        std::uint64_t offset;
+        one_bit_prefixed_varint::decode(std::istreambuf_iterator<char>(is), end_it, allele, offset);
+        total_offset += offset;
+        it->offset = total_offset;
+        it->status = (allele ? allele_status::has_alt : allele_status::is_missing);
+      }
+
+      return is.good();
     }
     //================================================================//
   }
