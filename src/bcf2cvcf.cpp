d08f1ea4e18af17d04eb827a5fcdfae12e1d5a0a
jonathonl <jonathonl@users.noreply.github.com>
2017-04-03 16:36:14 -0400

    Uses new api for bcf2cvcf.

--- src/bcf2cvcf.cpp
+++ src/bcf2cvcf.cpp
@@ -1,32 +1,30 @@
 
 #include "vcf_reader.hpp"
 #include "cmf_reader.hpp"
+#include "vc.hpp"
 
 #include <fstream>
 
 int main(int argc, char** argv)
 {
-
-
-  vc::vcf::block buff;
   vc::vcf::reader input(argv[1]);
-  vc::vcf::reader::input_iterator eof;
-  vc::vcf::reader::input_iterator cur(input, buff);
-  const std::string chrom = cur != eof ? vc::vcf::reader::get_chromosome(input, *cur) : "";
-  const int ploidy =  cur != eof ? cur->ploidy() : 0;
+  vc::vcf::dense_variant_iterator<float> cur(input);
+  vc::vcf::dense_variant_iterator<float> eof{};
+  const std::string chrom = cur != eof ? cur->chromosome() : "";
+  const int ploidy =  cur != eof ? vc::get_ploidy(input, *cur) : 0;
 
   std::vector<std::string> sample_ids(input.samples_end() - input.samples_begin());
   std::copy(input.samples_begin(), input.samples_end(), sample_ids.begin());
-  vc::cmf::writer compact_output(argv[2], vc::vcf::reader::get_chromosome(input, *cur), ploidy, sample_ids.begin(), sample_ids.end());
+  vc::cmf::writer compact_output(argv[2], chrom, ploidy, sample_ids.begin(), sample_ids.end());
 
   while (cur != eof)
   {
-    if (vc::vcf::reader::get_chromosome(input, *cur) != chrom)
+    if (cur->chromosome() != chrom)
     {
       std::cerr << "Multiple chromosomes encountered. CMF files can only contain one chromosome." << std::endl;
       return -1;
     }
-    compact_output << vc::cmf::marker(cur->pos(), cur->ref(), cur->alt(), cur->begin(), cur->end());
+    compact_output << *cur;
 
     ++cur;
   }
