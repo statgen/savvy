1b8b52d54bb016ada3b33648ec9893e31c1045fe
jonathonl <jonathonl@users.noreply.github.com>
2017-04-10 14:49:12 -0400

    Cleans up some commented out code.

--- include/sav_reader.hpp
+++ include/sav_reader.hpp
@@ -25,168 +25,10 @@ namespace savvy
 {
   namespace sav
   {
-//    class marker
-//    {
-//    public:
-//      struct sparse_vector_allele
-//      {
-//        std::uint64_t offset;
-//        allele_status status;
-//        sparse_vector_allele() = default;
-//        sparse_vector_allele(sparse_vector_allele&&) = default;
-//        sparse_vector_allele(const sparse_vector_allele&) = default;
-//        sparse_vector_allele& operator=(const sparse_vector_allele&) = default;
-//        sparse_vector_allele& operator=(sparse_vector_allele&&) = default;
-//        sparse_vector_allele(allele_status s, std::uint64_t o) : offset(o), status(s) {}
-//      };
-//
-//      typedef std::vector<sparse_vector_allele>::const_iterator non_ref_iterator;
-//
-//      class const_iterator
-//      {
-//      public:
-//        typedef const_iterator self_type;
-//        typedef std::ptrdiff_t difference_type;
-//        typedef allele_status value_type;
-//        typedef const value_type& reference;
-//        typedef const value_type* pointer;
-//        typedef std::forward_iterator_tag iterator_category;
-//      private:
-//        static const value_type const_is_missing;
-//        static const value_type const_has_ref;
-//        static const value_type const_has_alt;
-//      public:
-//        const_iterator(std::uint64_t off, const std::vector<sparse_vector_allele>::const_iterator& ptr, const std::vector<sparse_vector_allele>::const_iterator& ptr_end)
-//          : ptr_(ptr), ptr_end_(ptr_end), i_(off) {}
-//        void increment()
-//        {
-//          if (ptr_ != ptr_end_ && i_ == ptr_->offset)
-//            ++ptr_;
-//          ++i_;
-//        }
-//        self_type& operator++(){ increment(); return *this; }
-//        self_type operator++(int) { self_type r = *this; increment(); return r; }
-//        reference operator*()
-//        {
-//          if (ptr_ != ptr_end_ && i_ == ptr_->offset)
-//            return (ptr_->status == allele_status::is_missing ? const_is_missing : const_has_alt);
-//          return const_has_ref;
-//        }
-//        pointer operator->() { return &(const_iterator::operator*()); }
-//        bool operator==(const self_type& rhs) { return i_ == rhs.i_; }
-//        bool operator!=(const self_type& rhs) { return i_ != rhs.i_; }
-//      private:
-//        std::vector<sparse_vector_allele>::const_iterator ptr_;
-//        const std::vector<sparse_vector_allele>::const_iterator ptr_end_;
-//        std::size_t i_;
-//      };
-//
-//      marker() = default;
-//      template <typename RandAccessAlleleIterator>
-//      marker(std::uint64_t position, const std::string& ref, const std::string& alt, RandAccessAlleleIterator gt_beg, RandAccessAlleleIterator gt_end) :
-//        position_(position),
-//        ref_(ref),
-//        alt_(alt)
-//      {
-//        haplotype_count_ = gt_end - gt_beg;
-//        std::uint64_t off = 0;
-//        while (gt_beg != gt_end)
-//        {
-//          if (*gt_beg != allele_status::has_ref)
-//          {
-//            non_zero_haplotypes_.reserve(static_cast<std::size_t>(non_zero_haplotypes_.size() * 1.1f));
-//            non_zero_haplotypes_.emplace_back(*gt_beg, off);
-//          }
-//          ++gt_beg;
-//          ++off;
-//        }
-//        non_zero_haplotypes_.shrink_to_fit();
-//      }
-//
-//      template <typename RandAccessSparseAlleleIterator>
-//      marker(std::uint64_t position, const std::string& ref, const std::string& alt, RandAccessSparseAlleleIterator gt_beg, RandAccessSparseAlleleIterator gt_end, std::size_t total_haplotype_count) :
-//        position_(position),
-//        ref_(ref),
-//        alt_(alt),
-//        haplotype_count_(total_haplotype_count)
-//      {
-//        non_zero_haplotypes_.reserve(gt_end - gt_beg);
-//        while (gt_beg != gt_end)
-//        {
-//          if (gt_beg->status != allele_status::has_ref)
-//            non_zero_haplotypes_.emplace_back(*gt_beg);
-//          else
-//            throw new std::range_error("FOOBAR");
-//          ++gt_beg;
-//        }
-//        if (haplotype_count_ < non_zero_haplotypes_.size())
-//          throw new std::range_error("FOOBAR2");
-//        non_zero_haplotypes_.shrink_to_fit();
-//      }
-//
-//      std::uint64_t pos() const { return position_; }
-//      const std::string& ref() const { return ref_; }
-//      const std::string& alt() const { return alt_; }
-//      std::uint64_t haplotype_count() const { return haplotype_count_; }
-//      const allele_status& operator[](std::uint64_t i) const;
-//      const allele_status& at(std::uint64_t i) const;
-//      non_ref_iterator non_ref_begin() const;
-//      non_ref_iterator non_ref_end() const;
-//      const_iterator begin() const;
-//      const_iterator end() const;
-//      double calculate_allele_frequency() const;
-//      static void read(marker& destination, std::uint64_t haplotype_count, std::istream& is);
-//      static void write(std::ostream& os, const marker& source);
-//    private:
-//      std::vector<sparse_vector_allele> non_zero_haplotypes_;
-//      std::string ref_;
-//      std::string alt_;
-//      std::uint64_t position_;
-//      std::uint64_t haplotype_count_;
-//
-//
-//      std::size_t calculate_serialized_gt_size() const;
-//      std::tuple<std::size_t, std::size_t> calculate_rle_serialized_gt_size_and_count() const;
-//    };
 
     class reader_base
     {
     public:
-//      class input_iterator
-//      {
-//      public:
-//        typedef input_iterator self_type;
-//        typedef std::ptrdiff_t difference_type;
-//        typedef marker value_type;
-//        typedef const value_type& reference;
-//        typedef const value_type* pointer;
-//        typedef std::input_iterator_tag iterator_category;
-//        typedef marker buffer;
-//
-//        input_iterator() : file_reader_(nullptr), buffer_(nullptr) {}
-//        input_iterator(reader& file_reader, marker& buffer) :
-//          file_reader_(&file_reader),
-//          buffer_(&buffer)
-//        {
-//          increment();
-//        }
-//
-//        void increment()
-//        {
-//          bool b = file_reader_->good();
-//          if (!(*file_reader_ >> *buffer_))
-//            file_reader_ = nullptr;
-//        }
-//        self_type& operator++(){ increment(); return *this; }
-//        void operator++(int) { increment(); }
-//        reference operator*() { return *buffer_; }
-//        pointer operator->() { return buffer_; }
-//        bool operator==(const self_type& rhs) { return (file_reader_ == rhs.file_reader_); }
-//        bool operator!=(const self_type& rhs) { return (file_reader_ != rhs.file_reader_); }
-//      private:
-//        reader* file_reader_;
-//        marker* buffer_;
-//      };
 
       reader_base(const std::string& file_path);
       reader_base(reader_base&& source);
@@ -348,86 +190,6 @@ namespace savvy
     class indexed_reader : public reader_base
     {
     public:
-//      class region_query
-//      {
-//      public:
-//        class iterator
-//        {
-//        public:
-//          typedef iterator self_type;
-//          typedef std::ptrdiff_t difference_type;
-//          typedef marker value_type;
-//          typedef const value_type& reference;
-//          typedef const value_type* pointer;
-//          typedef std::bidirectional_iterator_tag iterator_category;
-//          typedef marker buffer;
-//
-//          iterator(s1r::reader::query::iterator&& idx_it) :
-//            parent_query_(nullptr),
-//            ifs_(nullptr),
-//            i_(idx_it)
-//          {
-//          }
-//
-//          iterator(region_query& parent, std::istream& is, s1r::reader::query::iterator&& idx_it) :
-//            parent_query_(&parent),
-//            ifs_(&is),
-//            i_(idx_it)
-//          {
-//            read_marker();
-//          }
-//
-//          self_type& operator++()
-//          {
-//            ++i_;
-//            read_marker();
-//            return *this;
-//          }
-//
-//          self_type operator++(int)
-//          {
-//            self_type r = *this;
-//            ++(*this);
-//            return r;
-//          }
-//          reference operator*() { return m_; }
-//          pointer operator->() { return &m_; }
-//          bool operator==(const self_type& rhs) { return (i_ == rhs.i_); }
-//          bool operator!=(const self_type& rhs) { return (i_ != rhs.i_); }
-//        private:
-//          region_query* parent_query_;
-//          std::istream* ifs_;
-//          s1r::reader::query::iterator i_;
-//          marker m_;
-//
-//          void read_marker()
-//          {
-//            ifs_->seekg(std::streampos(i_->value().first));
-//            parent_query_->parent_reader() >> m_;
-//            if (!parent_query_->parent_reader().good())
-//              *this = parent_query_->end();
-//          }
-//        };
-//
-//        region_query(reader& rdr, std::istream& is, s1r::reader::query&& idx_query) :
-//          reader_(&rdr),
-//          ifs_(&is),
-//          index_query_(std::move(idx_query))
-//        {
-//
-//        }
-//
-//        reader& parent_reader() { return *reader_; }
-//
-//        iterator begin() { return iterator(*this, *ifs_, index_query_.begin()); }
-//        iterator end() { return iterator(index_query_.end()); }
-//      private:
-//        reader* reader_;
-//        std::istream* ifs_;
-//        s1r::reader::query index_query_;
-//      };
-//
-
       indexed_reader(const std::string& file_path, std::uint64_t from, std::uint64_t to, const std::string& index_file_path = "") :
         reader_base(file_path),
         index_(index_file_path.size() ? index_file_path : file_path + ".s1r"),
@@ -498,20 +260,6 @@ namespace savvy
         ++i_;
         return true;
       }
-
-//
-//      region_query create_query(std::uint64_t start, std::uint64_t end = std::numeric_limits<std::uint64_t>::max())
-//      {
-//        return region_query(*this, this->input_stream_, index_.create_query(start, end));
-//      }
-//
-//      region_query create_query(const std::string& chromosome, std::uint64_t start, std::uint64_t end = std::numeric_limits<std::uint64_t>::max())
-//      {
-//        if (chromosome == this->chromosome())
-//          return this->create_query(start, end);
-//        else
-//          return region_query(*this, this->input_stream_, index_.create_query((std::uint64_t)-1, 0));
-//      }
     private:
       s1r::reader index_;
       s1r::reader::query query_;
@@ -656,12 +404,6 @@ namespace savvy
     {
       return std::strlen(str);
     }
-
-//    template <>
-//    inline std::size_t writer::get_string_size(char* str)
-//    {
-//      return std::strlen(str);
-//    }
 
     template <typename VecType>
     using variant_iterator =  basic_variant_iterator<reader_base, VecType>;
--- include/vcf_reader.hpp
+++ include/vcf_reader.hpp
@@ -18,10 +18,8 @@
 //{
 //namespace vcf
 //{
-#include "vcf.h"
 #include <synced_bcf_reader.h>
 #include <vcf.h>
-#include <dep/htslib_project-prefix/src/htslib_project/htslib/vcf.h>
 //}
 //}
 
@@ -30,155 +28,9 @@ namespace savvy
   namespace vcf
   {
 
-//    class marker
-//    {
-//    public:
-//      class const_iterator
-//      {
-//      public:
-//        typedef const_iterator self_type;
-//        typedef std::ptrdiff_t difference_type;
-//        typedef allele_status value_type;
-//        typedef const value_type& reference;
-//        typedef const value_type* pointer;
-//        typedef std::bidirectional_iterator_tag iterator_category;
-//      public:
-//        const_iterator(const marker& parent, std::uint64_t index) : parent_(&parent), cur_(index) {}
-//
-//        self_type& operator+=(difference_type n) { cur_ += n; return *this; }
-//        self_type operator+(savvy::vcf::marker::const_iterator::difference_type n) const { savvy::vcf::marker::const_iterator ret(*this); return (ret += n); }
-//        self_type& operator-=(difference_type n) { cur_ -= n; return *this; }
-//        self_type operator-(difference_type n) const { self_type ret(*this); return (ret -= n); }
-//        difference_type operator-(const self_type& b) const { return cur_ - b.cur_; }
-//
-//        self_type& operator--(){ --cur_; return *this; }
-//        self_type operator--(int) { self_type r = *this; --cur_; return r; }
-//        self_type& operator++(){ ++cur_; return *this; }
-//        self_type operator++(int) { self_type r = *this; ++cur_; return r; }
-//        reference operator*() { return (*parent_)[cur_]; }
-//        pointer operator->() { return &(*parent_)[cur_]; }
-//        bool operator==(const self_type& rhs) { return cur_ == rhs.cur_; }
-//        bool operator!=(const self_type& rhs) { return cur_ != rhs.cur_; }
-//      private:
-//        const marker* parent_;
-//        std::size_t cur_;
-//      };
-//
-//      marker(bcf1_t* hts_rec, int* gt, int num_gt, std::uint16_t allele_index);
-//      ~marker();
-//
-//      const allele_status& operator[](std::size_t i) const;
-//      std::uint64_t haplotype_count() const { return static_cast<std::uint64_t>(num_gt_); }
-//      int ploidy() const { return num_gt_ / hts_rec_->n_sample; }
-//      std::int32_t chrom_id() const;
-//      std::uint64_t pos() const;
-//      std::string ref() const;
-//      std::string alt() const;
-//      const_iterator begin() const { return const_iterator(*this, 0); }
-//      const_iterator end() const { return const_iterator(*this, haplotype_count()); }
-//    private:
-//      bcf1_t* hts_rec_;
-//      int* gt_;
-//      int num_gt_;
-//      std::uint32_t allele_index_;
-//
-//      static const allele_status const_is_missing;
-//      static const allele_status const_has_ref;
-//      static const allele_status const_has_alt;
-//    };
-
-//    class block
-//    {
-//    public:
-//      class const_iterator
-//      {
-//      public:
-//        typedef const_iterator self_type;
-//        typedef std::ptrdiff_t difference_type;
-//        typedef marker value_type;
-//        typedef const value_type& reference;
-//        typedef const value_type* pointer;
-//        typedef std::random_access_iterator_tag iterator_category;
-//
-//        const_iterator(pointer ptr) : ptr_(ptr) { }
-//        self_type& operator--() { --ptr_; return *this; }
-//        self_type operator--(int) { self_type r = *this; --ptr_; return r; }
-//        self_type& operator++() { ++ptr_; return *this; }
-//        self_type operator++(int) { self_type r = *this; ++ptr_; return r; }
-//        reference operator*() { return *ptr_; }
-//        pointer operator->() { return ptr_; }
-//        bool operator==(const self_type& rhs) { return ptr_ == rhs.ptr_; }
-//        bool operator!=(const self_type& rhs) { return ptr_ != rhs.ptr_; }
-//      private:
-//        pointer ptr_;
-//      };
-//
-//      block();
-//      block(const block& source) = delete;
-//      block(block&& source);
-//      block& operator=(const block& source) = delete;
-//      block& operator=(block&& source);
-//      ~block();
-//
-//      const_iterator begin() const { return const_iterator(this->markers_.data()); }
-//      const_iterator end() const { return const_iterator(this->markers_.data() + this->markers_.size()); }
-//      const marker& operator[](std::size_t i) const;
-//      std::size_t marker_count() const { return markers_.size(); }
-//      int sample_count() const { return num_samples_; }
-//      int ploidy() const { return gt_sz_ / num_samples_; }
-//
-//      static bool read_block(block& destination, htsFile* hts_file, bcf_hdr_t* hts_hdr);
-//      static bool read_block(block& destination, bcf_srs_t* sr);
-//    private:
-//      std::vector<marker> markers_;
-//      bcf1_t* hts_rec_;
-//      int* gt_;
-//      int gt_sz_;
-//      int num_samples_;
-//    };
-
     class reader_base
     {
     public:
-//      class input_iterator
-//      {
-//      public:
-//        typedef input_iterator self_type;
-//        typedef std::ptrdiff_t difference_type;
-//        typedef marker value_type;
-//        typedef const value_type& reference;
-//        typedef const value_type* pointer;
-//        typedef std::input_iterator_tag iterator_category;
-//        typedef block buffer;
-//
-//        input_iterator() : file_reader_(nullptr), buffer_(nullptr), i_(0) {}
-//        input_iterator(reader_base& file_reader, block& buffer) : file_reader_(&file_reader), buffer_(&buffer), i_(0)
-//        {
-//          if (!(*file_reader_ >> *buffer_))
-//            file_reader_ = nullptr;
-//        }
-//        void increment()
-//        {
-//          ++i_;
-//          if (i_ >= buffer_->marker_count())
-//          {
-//            i_ = 0;
-//            if (!(*file_reader_ >> *buffer_))
-//              file_reader_ = nullptr;
-//          }
-//        }
-//        self_type& operator++(){ increment(); return *this; }
-//        void operator++(int) { increment(); }
-//        reference operator*() { return (*buffer_)[i_]; }
-//        pointer operator->() { return &(*buffer_)[i_]; }
-//        bool operator==(const self_type& rhs) { return (file_reader_ == rhs.file_reader_); }
-//        bool operator!=(const self_type& rhs) { return (file_reader_ != rhs.file_reader_); }
-//      private:
-//        reader_base* file_reader_;
-//        block* buffer_;
-//        std::uint32_t i_;
-//      };
-
       reader_base() :
         state_(std::ios::goodbit),
         gt_(nullptr),
@@ -479,29 +331,6 @@ namespace savvy
     using dense_variant_iterator =  basic_variant_iterator<reader_base, std::vector<ValType>>;
     template <typename ValType>
     using sparse_variant_iterator =  basic_variant_iterator<reader_base, compressed_vector<ValType>>;
-
-//    template <typename PathItr, typename RegionItr>
-//    region_reader::region_reader(PathItr file_paths_beg, PathItr file_paths_end, RegionItr regions_beg, RegionItr regions_end) :
-//      synced_readers_(bcf_sr_init())
-//    {
-//      std::stringstream contigs;
-//      for (auto it = regions_beg; it != regions_end; )
-//      {
-//        contigs << it->chromosome();
-//        if (it->from() > 1 || it->to() != std::numeric_limits<std::uint64_t>::max())
-//          contigs << ":" << it->from() << "-" << it->to();
-//
-//        ++it;
-//        if (it != regions_end)
-//          contigs << ",";
-//      }
-//      bcf_sr_set_regions(synced_readers_, contigs.str().c_str(), 0);
-//
-//      for (auto it = file_paths_beg; it != file_paths_end; ++it)
-//      {
-//        bcf_sr_add_reader(synced_readers_, it->c_str());
-//      }
-//    }
   }
 }
 
