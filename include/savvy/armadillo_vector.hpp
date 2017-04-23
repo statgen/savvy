6113b6073b3c11d164a91dbd956bddaed99c26b4
jonathonl <jonathonl@users.noreply.github.com>
2017-04-22 21:58:21 -0400

    Updates helper vectors.

---
+++ include/savvy/armadillo_vector.hpp
@@ -0,0 +1,37 @@
+#ifndef LIBSAVVY_ARMADILLO_VECTOR_HPP
+#define LIBSAVVY_ARMADILLO_VECTOR_HPP
+
+#include "allele_vector.hpp"
+
+#include <armadillo>
+#include <cstddef>
+
+namespace savvy
+{
+  template <typename T>
+  class armadillo_dense_vector : public arma::Mat<T>
+  {
+  public:
+    typedef T value_type;
+    using arma::Mat<T>::Mat;
+
+    void resize(std::size_t sz)
+    {
+      std::size_t before_size = arma::Mat<T>::size();
+      arma::Mat<T>::resize(sz);
+      if (arma::Mat<T>::size() > before_size)
+      {
+        for (std::size_t i = before_size; i < arma::Mat<T>::size(); ++i)
+          (*this)[i] = value_type();
+      }
+    }
+  };
+
+//  template <typename T>
+//  using sparse_armadillo_allele_vector = allele_vector<arma::sp_mat<T>>;
+
+  template <typename T>
+  using dense_armadillo_allele_vector = allele_vector<armadillo_dense_vector<T>>;
+}
+
+#endif //LIBSAVVY_ARMADILLO_VECTOR_HPP
--- include/savvy/eigen3_vector.hpp
+++ include/savvy/eigen3_vector.hpp
@@ -2,8 +2,11 @@
 #ifndef LIBSAVVY_EIGEN3_VECTOR_HPP
 #define LIBSAVVY_EIGEN3_VECTOR_HPP
 
+#include "allele_vector.hpp"
+
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
+#include <cstddef>
 
 namespace savvy
 {
@@ -49,9 +52,15 @@ namespace savvy
       return Eigen::Matrix<T, 1, Eigen::Dynamic>::coeffRef(idx);
     }
 
-    void resize(std::size_t size)
+    void resize(std::size_t sz)
     {
-      Eigen::Matrix<T, 1, Eigen::Dynamic>::setConstant(size, 0);
+      std::size_t before_size = Eigen::Matrix<T, 1, Eigen::Dynamic>::size();
+      Eigen::Matrix<T, 1, Eigen::Dynamic>::resize(sz);
+      if (Eigen::Matrix<T, 1, Eigen::Dynamic>::size() > before_size)
+      {
+        for (std::size_t i = before_size; i < Eigen::Matrix<T, 1, Eigen::Dynamic>::size(); ++i)
+          (*this)[i] = value_type();
+      }
     }
   };
 
@@ -59,6 +68,6 @@ namespace savvy
   using sparse_eigen3_allele_vector = allele_vector<eigen3_sparse_vector<T>>;
 
   template <typename T>
-  using dense_eigen3_allele_vector = allele_vector<dense_sparse_vector<T>>;
+  using dense_eigen3_allele_vector = allele_vector<eigen3_dense_vector<T>>;
 }
 #endif //LIBSAVVY_EIGEN3_VECTOR_HPP
