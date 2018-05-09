ded80112dfc56d0ebb7a483758b55e356163d443
jonathonl <jonathonl@users.noreply.github.com>
2018-05-09 17:05:31 -0400

    Fixes finding cget version of zlib.

--- dep/htslib.cmake
+++ dep/htslib.cmake
@@ -3,13 +3,20 @@ project(htslib VERSION 1.3.1)
 
 get_property(dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
 foreach(dir ${dirs})
-    set(CMAKE_C_FLAGS "-I${dir}" ${CMAKE_C_FLAGS})
-    set(CMAKE_CXX_FLAGS "-I${dir}" ${CMAKE_CXX_FLAGS})
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I${dir}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I${dir}")
 endforeach()
 message("dirs: ${dirs}")
 message("cxxflags: ${CMAKE_CXX_FLAGS}")
 
+#list(APPEND CMAKE_C_FLAGS ${dirs})
+#list(APPEND CMAKE_CXX_FLAGS ${dirs})
+
 #execute_process(COMMAND ./configure --disable-libcurl --disable-lzma --disable-bz2 --prefix=${CMAKE_INSTALL_PREFIX} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+set(ENV{CFLAGS}  "${CMAKE_C_FLAGS}")
+set(ENV{CXXFLAGS} "${CMAKE_CXX_FLAGS}")
+set(ENV{LDFLAGS} "-L${CMAKE_PREFIX_PATH}/lib")
 
 #set(ENV{CFLAGS} "${CMAKE_C_FLAGS} $ENV{CFLAGS}")
 #set(ENV{CXXFLAGS} "${CMAKE_CXX_FLAGS} $ENV{CXXFLAGS}")
@@ -18,7 +25,7 @@ message("cxxflags: ${CMAKE_CXX_FLAGS}")
 
 execute_process(COMMAND ./configure --disable-libcurl --disable-lzma --disable-bz2 --prefix=${CMAKE_INSTALL_PREFIX} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}hts${CMAKE_SHARED_LIBRARY_SUFFIX}" "${CMAKE_CURRENT_SOURCE_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}hts${CMAKE_STATIC_LIBRARY_SUFFIX}"
-                   COMMAND $(MAKE) CFLAGS="${CMAKE_C_FLAGS}" CXXFLAGS="${CMAKE_CXX_FLAGS}"
+                   COMMAND $(MAKE)
                    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                    COMMENT "Building htslib ...")
 add_custom_target(hts ALL DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}hts${CMAKE_SHARED_LIBRARY_SUFFIX}" "${CMAKE_CURRENT_SOURCE_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}hts${CMAKE_STATIC_LIBRARY_SUFFIX}")
