a4d301908d7dc5402678cd1360cfc50a441b927b
jonathonl <jonathonl@users.noreply.github.com>
2019-01-11 10:37:56 -0500

    Reorganizes variant_group_iterator.

--- include/savvy/variant_group_iterator.hpp
+++ include/savvy/variant_group_iterator.hpp
@@ -18,8 +18,91 @@
 
 namespace savvy
 {
+  class variant_group_iterator_util
+  {
+  public:
+    static std::vector<region> merge_regions(const std::vector<region>& un_merged_regions)
+    {
+      std::vector<region> ret;
+
+      for (auto it = un_merged_regions.begin(); it != un_merged_regions.end(); ++it)
+      {
+        if (ret.empty() || ret.back().chromosome() != it->chromosome())
+        {
+          ret.emplace_back(*it);
+        }
+        else
+        {
+          std::uint64_t from = std::min(ret.back().from(), it->from());
+          std::uint64_t to = std::max(ret.back().to(), it->to());
+          ret.back() = region(ret.back().chromosome(), from, to);
+        }
+      }
+
+      return ret;
+    }
+
+    // [CHROM]:[POS]_[REF]/[ALT]
+    static savvy::site_info marker_id_to_site_info(std::string::const_iterator beg, std::string::const_iterator end)
+    {
+      auto colon_it = std::find(beg, end, ':');
+      std::string chrom(beg, colon_it);
+      if (colon_it != end)
+      {
+        auto underscore_it = std::find(++colon_it, end, '_');
+        std::uint64_t pos = static_cast<std::uint64_t>(std::atoll(std::string(colon_it, underscore_it).c_str()));
+        if (underscore_it != end)
+        {
+          auto slash_it = std::find(++underscore_it, end, '/');
+          std::string ref(underscore_it, slash_it);
+          if (slash_it != end)
+          {
+            std::string alt(++slash_it, end);
+            return savvy::site_info{std::move(chrom), pos, std::move(ref), std::move(alt), {}};
+          }
+        }
+      }
+
+      return savvy::site_info{};
+    }
+
+    // [CHROM]:[POS]_[REF]/[ALT]
+    static savvy::region site_info_to_region(const savvy::site_info& site)
+    {
+      std::size_t length = std::max(site.ref().size(), site.alt().size());
+      if (length > 0)
+      {
+        return savvy::region{site.chromosome(), site.position(), site.position() + length - 1};
+      }
+
+      return savvy::region{""};
+    }
+
+    static std::tuple<std::string, std::list<savvy::site_info>> parse_marker_group_line(const std::string& input)
+    {
+      std::tuple<std::string, std::list<savvy::site_info>> ret;
+      auto delim_it = std::find(input.begin(), input.end(), '\t');
+      if (delim_it != input.end())
+      {
+        std::get<0>(ret) = std::string(input.begin(), delim_it);
+        ++delim_it;
+
+        std::string::const_iterator next_delim_it;
+        while ((next_delim_it = std::find(delim_it, input.end(), '\t')) != input.end())
+        {
+          std::get<1>(ret).emplace_back(marker_id_to_site_info(delim_it, next_delim_it));
+          delim_it = next_delim_it + 1;
+        }
+
+        std::get<1>(ret).emplace_back(marker_id_to_site_info(delim_it, input.end()));
+      }
+
+      return ret;
+    }
+  };
+
   template<typename RdrT, typename VecT>
-  class basic_variant_group_iterator
+  class basic_variant_group_iterator : public variant_group_iterator_util
   {
   public:
     typedef basic_variant_group_iterator<RdrT, VecT> self_type;
@@ -151,85 +234,6 @@ namespace savvy
       }
 
       rdr_ = nullptr;
-    }
-
-    std::vector<region> merge_regions(const std::vector<region>& un_merged_regions)
-    {
-      std::vector<region> ret;
-
-      for (auto it = un_merged_regions.begin(); it != un_merged_regions.end(); ++it)
-      {
-        if (ret.empty() || ret.back().chromosome() != it->chromosome())
-        {
-          ret.emplace_back(*it);
-        }
-        else
-        {
-          std::uint64_t from = std::min(ret.back().from(), it->from());
-          std::uint64_t to = std::max(ret.back().to(), it->to());
-          ret.back() = region(ret.back().chromosome(), from, to);
-        }
-      }
-
-      return ret;
-    }
-
-    // [CHROM]:[POS]_[REF]/[ALT]
-    static savvy::site_info marker_id_to_site_info(std::string::const_iterator beg, std::string::const_iterator end)
-    {
-      auto colon_it = std::find(beg, end, ':');
-      std::string chrom(beg, colon_it);
-      if (colon_it != end)
-      {
-        auto underscore_it = std::find(++colon_it, end, '_');
-        std::uint64_t pos = static_cast<std::uint64_t>(std::atoll(std::string(colon_it, underscore_it).c_str()));
-        if (underscore_it != end)
-        {
-          auto slash_it = std::find(++underscore_it, end, '/');
-          std::string ref(underscore_it, slash_it);
-          if (slash_it != end)
-          {
-            std::string alt(++slash_it, end);
-            return savvy::site_info{std::move(chrom), pos, std::move(ref), std::move(alt), {}};
-          }
-        }
-      }
-
-      return savvy::site_info{};
-    }
-
-    // [CHROM]:[POS]_[REF]/[ALT]
-    static savvy::region site_info_to_region(const savvy::site_info& site)
-    {
-      std::size_t length = std::max(site.ref().size(), site.alt().size());
-      if (length > 0)
-      {
-        return savvy::region{site.chromosome(), site.position(), site.position() + length - 1};
-      }
-
-      return savvy::region{""};
-    }
-
-    static std::tuple<std::string, std::list<savvy::site_info>> parse_marker_group_line(const std::string& input)
-    {
-      std::tuple<std::string, std::list<savvy::site_info>> ret;
-      auto delim_it = std::find(input.begin(), input.end(), '\t');
-      if (delim_it != input.end())
-      {
-        std::get<0>(ret) = std::string(input.begin(), delim_it);
-        ++delim_it;
-
-        std::string::const_iterator next_delim_it;
-        while ((next_delim_it = std::find(delim_it, input.end(), '\t')) != input.end())
-        {
-          std::get<1>(ret).emplace_back(marker_id_to_site_info(delim_it, next_delim_it));
-          delim_it = next_delim_it + 1;
-        }
-
-        std::get<1>(ret).emplace_back(marker_id_to_site_info(delim_it, input.end()));
-      }
-
-      return ret;
     }
 
   private:
